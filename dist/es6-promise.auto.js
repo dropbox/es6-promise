66a5abe72b77c049396b17e232047a9e17814370
Linjie Ding <linjie@dropbox.com>
2016-12-29 17:36:28 -0800

    Added Promise.prototype.finally

--- dist/es6-promise.auto.js
+++ dist/es6-promise.auto.js
@@ -3,7 +3,7 @@
  * @copyright Copyright (c) 2014 Yehuda Katz, Tom Dale, Stefan Penner and contributors (Conversion to ES6 API by Jake Archibald)
  * @license   Licensed under MIT license
  *            See https://raw.githubusercontent.com/stefanpenner/es6-promise/master/LICENSE
- * @version   4.0.5+6d84d7f6
+ * @version   4.0.5+58da1cde
  */
 
 (function (global, factory) {
@@ -1115,6 +1115,61 @@ Promise.prototype = {
   */
   'catch': function _catch(onRejection) {
     return this.then(null, onRejection);
+  },
+
+  /**
+    `finally` will be invoked regardless of the promise's fate just as native
+    try/catch/finally behaves
+  
+    Synchronous example:
+  
+    ```js
+    findAuthor() {
+      if (Math.random() > 0.5) {
+        throw new Error();
+      }
+      return new Author();
+    }
+  
+    try {
+      return findAuthor(); // succeed or fail
+    } catch(error) {
+      return findOtherAuthor();
+    } finally {
+      // always runs
+      // doesn't affect the return value
+    }
+    ```
+  
+    Asynchronous example:
+  
+    ```js
+    findAuthor().catch(function(reason){
+      return findOtherAuthor();
+    }).finally(function(){
+      // author was either found, or not
+    });
+    ```
+  
+    @method finally
+    @param {Function} callback
+    @param {String} label optional string for labeling the promise.
+    Useful for tooling.
+    @return {Promise}
+  */
+  'finally': function _finally(callback, label) {
+    var promise = this;
+    var constructor = promise.constructor;
+
+    return promise.then(function (value) {
+      return constructor.resolve(callback()).then(function () {
+        return value;
+      });
+    }, function (reason) {
+      return constructor.resolve(callback()).then(function () {
+        throw reason;
+      });
+    });
   },
 
   // Ported from RSVP.js
--- test/extension-test.js
+++ test/extension-test.js
@@ -1242,7 +1242,7 @@ describe("Promise.onerror", function(){
   var onerror;
 
   afterEach(function() {
-    Promise.onerror = function(){};
+    Promise.onerror = null;
   });
 
   it("When provided, any unhandled exceptions are sent to it (false positive case)", function(done) {
@@ -1392,6 +1392,240 @@ describe("Promise.onerror", function(){
       promise.catch(function() {
         done();
       });
+    });
+  });
+
+  it("finally should not 'handle' errors (1)", function(done) {
+    var thrownError = new Error();
+
+    Promise.onerror = function(event) {
+      assert(true, 'Should get here');
+      done();
+    };
+
+    var promise = new Promise(function(resolve, reject) {
+      reject(thrownError);
+    });
+
+    promise.finally(function(error) {
+
+    });
+  });
+
+  it("finally should not 'handle' errors (2)", function(done) {
+    var thrownError = new Error();
+
+    Promise.onerror = function(event) {
+      assert(true, 'Should get here');
+      done();
+    };
+
+    var promise = new Promise(function(resolve, reject) {
+      reject(thrownError);
+    });
+
+    promise.then().finally(function(error) {
+
+    });
+  });
+
+  it("finally should not 'handle' errors (3)", function(done) {
+    var thrownError = new Error();
+
+    Promise.onerror = function(event) {
+      assert(false, 'Should not get here');
+      done();
+    };
+
+    var promise = new Promise(function(resolve, reject) {
+      reject(thrownError);
+    });
+
+    promise.then().finally(function(error) {
+
+    }).then(null, function() {
+      assert(true);
+      done();
+    });
+  });
+
+  it("finally should trigger onError when its callback throws", function(done) {
+    var thrownError = new Error();
+    var finallyError = new Error("OMG");
+
+    Promise.onerror = function(event) {
+      assert(true, 'Should get here');
+      assert.equal(finallyError, event, "correct error is triggered");
+      done();
+    };
+
+    var promise = new Promise(function(resolve, reject) {
+      reject(thrownError);
+    });
+
+    promise.then().finally(function(error) {
+      throw finallyError;
+    });
+  });
+
+  it("finally should trigger onError when its callback rejects", function(done) {
+    var thrownError = new Error();
+    var finallyError = new Error("OMG");
+
+    Promise.onerror = function(event) {
+      assert(true, 'Should get here');
+      assert.equal(finallyError, event, "correct error is triggered");
+      done();
+    };
+
+    var promise = new Promise(function(resolve, reject) {
+      reject(thrownError);
+    });
+
+    promise.then().finally(function(error) {
+      return Promise.reject(finallyError);
+    });
+  });
+});
+
+describe("Promise.prototype.finally", function() {
+  describe("native finally behaviour", function() {
+    describe("no value is passed in", function() {
+      it("does not provide a value to the finally code", function(done) {
+        var fulfillmentValue = 1;
+        var promise = Promise.resolve(fulfillmentValue);
+
+        promise['finally'](function() {
+          assert.equal(arguments.length, 0);
+          done();
+        });
+      });
+
+      it("does not provide a reason to the finally code", function(done) {
+        var rejectionReason = new Error();
+        var promise = Promise.reject(rejectionReason);
+
+        promise['finally'](function(arg) {
+          assert.equal(arguments.length, 0);
+          done();
+        });
+      });
+    });
+
+    describe("non-exceptional cases do not affect the result", function(){
+      it("preserves the original fulfillment value even if the finally callback returns a value", function(done) {
+        var fulfillmentValue = 1;
+        var promise = Promise.resolve(fulfillmentValue);
+
+        promise['finally'](function() {
+          return 2;
+        }).then(function(value) {
+          assert.equal(fulfillmentValue, value);
+          done();
+        });
+      });
+
+      it("preserves the original rejection reason even if the finally callback returns a value", function(done) {
+        var rejectionReason = new Error();
+        var promise = Promise.reject(rejectionReason);
+
+        promise['finally'](function() {
+          return 2;
+        }).then(undefined, function(reason) {
+          assert.equal(rejectionReason, reason);
+          done();
+        });
+      });
+    });
+
+    describe("exception cases do propogate the failure", function(){
+      describe("fulfilled promise", function(){
+        it("propagates changes via throw", function(done) {
+          var promise = Promise.resolve(1);
+          var expectedReason  = new Error();
+
+          promise['finally'](function() {
+            throw expectedReason;
+          }).then(undefined, function(reason) {
+            assert.deepEqual(expectedReason, reason);
+            done();
+          });
+        });
+
+        it("propagates changes via returned rejected promise", function(done){
+          var promise = Promise.resolve(1);
+          var expectedReason  = new Error();
+
+          promise['finally'](function() {
+            return Promise.reject(expectedReason);
+          }).then(undefined, function(reason) {
+            assert.deepEqual(expectedReason, reason);
+            done();
+          });
+        });
+      });
+
+      describe("rejected promise", function(){
+        it("propagates changes via throw", function(done) {
+          var promise = Promise.reject(1);
+          var expectedReason  = new Error();
+
+          promise['finally'](function() {
+            throw expectedReason;
+          }).then(undefined, function(reason) {
+            assert.deepEqual(expectedReason, reason);
+            done();
+          });
+        });
+
+        it("propagates changes via returned rejected promise", function(done){
+          var promise = Promise.reject(1);
+          var expectedReason  = new Error();
+
+          promise['finally'](function() {
+            return Promise.reject(expectedReason);
+          }).then(undefined, function(reason) {
+            assert.deepEqual(expectedReason, reason);
+            done();
+          });
+        });
+      });
+    });
+  });
+
+  describe("inheritance", function() {
+    function Subclass (resolver) {
+      this._promise$constructor(resolver);
+    }
+
+    Subclass.prototype = Object.create(Promise.prototype);
+    Subclass.prototype.constructor = Subclass;
+    Subclass.prototype._promise$constructor = Promise;
+
+    Subclass.resolve = Promise.resolve;
+    Subclass.reject = Promise.reject;
+    Subclass.all = Promise.all;
+
+    it("preserves correct subclass when chained", function() {
+      var promise = Subclass.resolve().finally();
+      assert.ok(promise instanceof Subclass);
+      assert.equal(promise.constructor, Subclass);
+    });
+
+    it("preserves correct subclass when rejected", function() {
+      var promise = Subclass.resolve().finally(function() {
+        throw new Error("OMG");
+      });
+      assert.ok(promise instanceof Subclass);
+      assert.equal(promise.constructor, Subclass);
+    });
+
+    it("preserves correct subclass when someone returns a thenable", function() {
+      var promise = Subclass.resolve().finally(function() {
+        return Promise.resolve(1);
+      });
+      assert.ok(promise instanceof Subclass);
+      assert.equal(promise.constructor, Subclass);
     });
   });
 });
