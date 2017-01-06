fd5149c8d35aa9a37bb417ae9b0c558b34d36d67
Linjie Ding <linjie@dropbox.com>
2017-01-06 00:12:55 -0800

    add fork repo url

--- dist/es6-promise.js
+++ dist/es6-promise.js
@@ -1,9 +1,10 @@
 /*!
- * @overview es6-promise - a tiny implementation of Promises/A+.
+ * @overview es6-promise - a tiny implementation of Promises/A+ (fork by Dropbox).
  * @copyright Copyright (c) 2014 Yehuda Katz, Tom Dale, Stefan Penner and contributors (Conversion to ES6 API by Jake Archibald)
  * @license   Licensed under MIT license
  *            See https://raw.githubusercontent.com/stefanpenner/es6-promise/master/LICENSE
- * @version   4.0.5+58da1cde
+ * @repo      https://github.com/dropbox/es6-promise
+ * @version   4.0.5+7b4bf22a
  */
 
 (function (global, factory) {
